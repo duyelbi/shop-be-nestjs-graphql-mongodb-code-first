80baeec8f876e3ba36207b91669cc98ced90ab6c
DuyElbi <duylanh1818@gmail.com>
2023-04-14 16:50:51 +0700

    add express

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,21 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import * as express from 'express';
 
 import { AppModule } from './app.module';
-
-import express from 'express';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const limit = configService.get<string>('app.limit');
+  app.use(express.urlencoded({ extended: true, limit: `${limit}` }));
+  app.use(express.json({ limit: `${limit}` }));
+  app.enableCors({
+    origin: function (origin, callback) {
+      callback(null, true);
+    },
+  });
   app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(process.env.PORT || 8080);
